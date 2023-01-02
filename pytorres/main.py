4116e48ab26ac656c370ae1d8a04890f35b4b12e
zwanto <antoine.hamon@protonmail.com>
2023-01-02 09:26:05 +0000

    maj

--- pytorres/main.py
+++ pytorres/main.py
@@ -1,18 +1,24 @@
 import os
-from itertools import groupby
+from datetime import timedelta
+from enum import Enum
+from itertools import accumulate
+from time import sleep
 from typing import Union
 
-import requests
 from dotenv import dotenv_values
 from elasticsearch import Elasticsearch
 from fastapi import Depends, FastAPI, HTTPException
-from fastapi_cache import FastAPICache
-from fastapi_cache.backends.redis import RedisBackend
-from fastapi_cache.decorator import cache
-from redis import asyncio as aioredis
+from requests_cache import CachedSession
+from requests_cache.backends import RedisCache
 
 from pytorres.parser import Parser
 from pytorres.utils import aggregate_es_scores, check_es
+
+
+class TitleType(str, Enum):
+    tv = "tv"
+    movie = "movie"
+
 
 config = {
     **dotenv_values(),
@@ -47,11 +53,14 @@ def es_get_detail(imdbId, index="imdb-movie*"):
 #     ╚═════╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝╚══════╝
 
 
-@app.on_event("startup")
-async def startup():
-    redis = aioredis.from_url(config['REDIS_HOST'], encoding="utf8", decode_responses=True)
-    FastAPICache.init(RedisBackend(redis), prefix="fastapi-cache")
-
+backend = RedisCache(host=config['REDIS_HOST'], port=int(config['REDIS_PORT']))
+session = CachedSession(
+    name='tmdb_cache', 
+    backend=backend,
+    allowable_codes=(200,),
+    allowable_methods=('GET',),
+    expire_after=timedelta(days=30),
+    )
 
 #    ███████╗███╗   ██╗██████╗ ██████╗  ██████╗ ██╗███╗   ██╗████████╗███████╗
 #    ██╔════╝████╗  ██║██╔══██╗██╔══██╗██╔═══██╗██║████╗  ██║╚══██╔══╝██╔════╝
@@ -71,17 +80,23 @@ def es_info():
     return es.info()
 
 
-#        ██╗███╗   ███╗ ██████╗ ██╗   ██╗██╗███████╗
-#       ██╔╝████╗ ████║██╔═══██╗██║   ██║██║██╔════╝
-#      ██╔╝ ██╔████╔██║██║   ██║██║   ██║██║█████╗  
-#     ██╔╝  ██║╚██╔╝██║██║   ██║╚██╗ ██╔╝██║██╔══╝  
-#    ██╔╝   ██║ ╚═╝ ██║╚██████╔╝ ╚████╔╝ ██║███████╗
-#    ╚═╝    ╚═╝     ╚═╝ ╚═════╝   ╚═══╝  ╚═╝╚══════╝
+@app.get("/parser/{parser}")
+def parse(parser: str, q: str):
+    q_parsed = parsers[parser](q)
+    return {'q_parsed': q_parsed.query_string, 'PTN': q_parsed.PTN}
 
 
-@app.get("/movie/detail/{imdbId}", dependencies=[Depends(check_es(es))])
-def movie(imdbId):
-    es_matches_primary = es_get_detail(imdbId,"imdb-movie*")
+#       ██╗███╗   ███╗ ██████╗ ██╗   ██╗██╗███████╗                  ██╗████████╗██╗   ██╗
+#      ██╔╝████╗ ████║██╔═══██╗██║   ██║██║██╔════╝                 ██╔╝╚══██╔══╝██║   ██║
+#     ██╔╝ ██╔████╔██║██║   ██║██║   ██║██║█████╗      █████╗      ██╔╝    ██║   ██║   ██║
+#    ██╔╝  ██║╚██╔╝██║██║   ██║╚██╗ ██╔╝██║██╔══╝      ╚════╝     ██╔╝     ██║   ╚██╗ ██╔╝
+#   ██╔╝   ██║ ╚═╝ ██║╚██████╔╝ ╚████╔╝ ██║███████╗              ██╔╝      ██║    ╚████╔╝ 
+#   ╚═╝    ╚═╝     ╚═╝ ╚═════╝   ╚═══╝  ╚═╝╚══════╝              ╚═╝       ╚═╝     ╚═══╝  
+                                                                                      
+
+@app.get("/{titleType}/detail/{imdbId}", dependencies=[Depends(check_es(es))])
+def es_lookup(titleType: TitleType, imdbId: str):
+    es_matches_primary = es_get_detail(imdbId,f"imdb-{titleType}*")
 
     if es_matches_primary['hits']['total']['value'] == 0:
         raise HTTPException(status_code=500, detail=f"No match found using imdbId {imdbId}.")
@@ -89,29 +104,21 @@ def movie(imdbId):
         return es_matches_primary['hits']['hits'][0]['_source']
 
 
-@app.get("/movie/detail/{imdbId}/tmdb")
-@cache(expire=60*60*24)
-async def movie_tmdb(imdbId: str):
+@app.get("/{titleType}/detail/{imdbId}/tmdb")
+async def tmdb_lookup(titleType: TitleType, imdbId: str):
 
-    res = requests.get(f'https://api.themoviedb.org/3/find/{imdbId}?api_key={ config.get("TMDB_API_KEY") }&external_source=imdb_id').json()
+    res = session.get(f'https://api.themoviedb.org/3/find/{imdbId}?api_key={ config.get("TMDB_API_KEY") }&external_source=imdb_id').json()
     
-    if len(res['movie_results']) == 0:
+    if len(res[f'{titleType}_results']) == 0:
         raise HTTPException(status_code=500, detail=f"No match found with imdbId {imdbId}.")
     else:
-        tmdbId = res['movie_results'][0]['id']
-        res = requests.get(f'https://api.themoviedb.org/3/movie/{tmdbId}?api_key={ config.get("TMDB_API_KEY") }&language=fr').json()
+        tmdbId = res[f'{titleType}_results'][0]['id']
+        res = session.get(f'https://api.themoviedb.org/3/{titleType}/{tmdbId}?api_key={ config.get("TMDB_API_KEY") }&language=fr').json()
         return res
 
 
-@app.get("/movie/lookup", dependencies=[Depends(check_es(es))])
-def movie_lookup(q: str, index: str, year: Union[None, str]=None, parser: Union[None, str]=None):
-
-    if parser is not None:
-        q_parsed = parsers[parser](q)
-        q = q_parsed.query_string
-
-        if year is None:
-            year = q_parsed.PTN.get('year')
+@app.get("/{titleType}/lookup", dependencies=[Depends(check_es(es))])
+def title_lookup(titleType: TitleType, q: str, index: str, year: Union[None, str]=None):
 
     query = {    
         "bool": {
@@ -121,67 +128,55 @@ def movie_lookup(q: str, index: str, year: Union[None, str]=None, parser: Union[
         }
     }
 
-    if year is not None:
+    if year is not None and year != '':
         query['bool']['should'] = [
             {"match": { "year": year }}
         ]
 
-    return aggregate_es_scores(es.search(index="imdb-movie*", query=query))
+    return aggregate_es_scores(es.search(index=f"imdb-{titleType}*", query=query))
 
 
-#       ██╗████████╗██╗   ██╗
-#      ██╔╝╚══██╔══╝██║   ██║
-#     ██╔╝    ██║   ██║   ██║
-#    ██╔╝     ██║   ╚██╗ ██╔╝
-#   ██╔╝      ██║    ╚████╔╝ 
-#   ╚═╝       ╚═╝     ╚═══╝  
-                       
-@app.get("/tv/detail/{imdbId}", dependencies=[Depends(check_es(es))])
-def tv(imdbId):
+#       ██╗ █████╗ ███╗   ██╗██╗███╗   ███╗███████╗
+#      ██╔╝██╔══██╗████╗  ██║██║████╗ ████║██╔════╝
+#     ██╔╝ ███████║██╔██╗ ██║██║██╔████╔██║█████╗  
+#    ██╔╝  ██╔══██║██║╚██╗██║██║██║╚██╔╝██║██╔══╝  
+#   ██╔╝   ██║  ██║██║ ╚████║██║██║ ╚═╝ ██║███████╗
+#   ╚═╝    ╚═╝  ╚═╝╚═╝  ╚═══╝╚═╝╚═╝     ╚═╝╚══════╝
 
-    es_matches_primary = es_get_detail(imdbId,"imdb-serie*")    
+@app.get("/{titleType}/detail/{imdbId}/tmdb/season/{season}/episode/{episode}")
+async def tmdb_lookup(imdbId: str, season: int, episode: int):
 
-    if es_matches_primary['hits']['total']['value'] == 0:
-        raise HTTPException(status_code=500, detail=f"No match found using imdbId {imdbId}.")
-    else:
-        return es_matches_primary['hits']['hits'][0]['_source']
+    res = session.get(f'https://api.themoviedb.org/3/find/{imdbId}?api_key={ config.get("TMDB_API_KEY") }&external_source=imdb_id')
+    assert res.status_code == 200
+    res = res.json()
 
-
-@app.get("/tv/detail/{imdbId}/tmdb")
-@cache(expire=60*60*24)
-async def tv_tmdb(imdbId: str):
-
-    res = requests.get(f'https://api.themoviedb.org/3/find/{imdbId}?api_key={ config.get("TMDB_API_KEY") }&external_source=imdb_id').json()
-    
-    if len(res['tv_results']) == 0:
+    if len(res[f'tv_results']) == 0:
         raise HTTPException(status_code=500, detail=f"No match found with imdbId {imdbId}.")
-    else:
-        tmdbId = res['tv_results'][0]['id']
-        res = requests.get(f'https://api.themoviedb.org/3/tv/{tmdbId}?api_key={ config.get("TMDB_API_KEY") }&language=fr').json()
-        return res
 
+    tmdbId = res[f'tv_results'][0]['id']
 
-@app.get("/tv/lookup", dependencies=[Depends(check_es(es))])
-def movie_lookup(q: str, index: str, year: Union[None, str]=None, parser: Union[None, str]=None):
+    if season == -1:
+        
+        r_tv = session.get(f'https://api.themoviedb.org/3/tv/{tmdbId}?api_key={ config.get("TMDB_API_KEY") }&language=fr&append_to_response=season')
+        assert r_tv.status_code == 200
+        data_tv = r_tv.json()
 
-    if parser is not None:
-        q_parsed = parsers[parser](q)
-        q = q_parsed.query_string
+        _ep = 0
+        for s in data_tv['seasons']:
+            if s['season_number'] == 0:
+                continue
+            _ep += s['episode_count']
+            print(_ep)
+            if episode <= _ep:
+                break
+        season = s['season_number']
 
-        if year is None:
-            year = q_parsed.PTN.get('year')
+    for i in range(season, season+5):
+        try:
+            r_ep = session.get(f'https://api.themoviedb.org/3/tv/{tmdbId}/season/{i}/episode/{episode}?api_key={ config.get("TMDB_API_KEY") }&language=fr')
+            assert r_ep.status_code == 200
+            break
+        except:
+            continue
 
-    query = {    
-        "bool": {
-            "must": [
-                {"query_string": { "query": q }}
-            ]
-        }
-    }
-
-    if year is not None:
-        query['bool']['should'] = [
-            {"match": { "year": year }}
-        ]
-
-    return aggregate_es_scores(es.search(index="imdb-serie*", query=query))
+    return r_ep.json()
--- pytorres/regex.py
+++ pytorres/regex.py
@@ -5,7 +5,7 @@ from pathlib import Path
 exclude_terms = open(Path(pytorres.__path__[0]) / 'conf/exclude_terms').readlines()
 exclude_terms = [i.strip() for i in exclude_terms]
 
-RE_CLEANUP =  re.compile('[\[\]\-]')
+RE_CLEANUP =  re.compile("[\[\]\-'{}]")
 RE_NOISE = re.compile(f"(?<!\w)(?:{'|'.join(exclude_terms)})(?!\w)", flags=re.IGNORECASE)
 RE_COMPRESS = re.compile(' +')
 
